fb0c3e076a89fa71c973f8e7617850d4da216241
pascal <madukwepascal@gmail.com>
2022-07-17 11:01:25 +0100

    Fix some bugs that malfunctions the calculator
    - Fix bug that makes it possible for two equals signs to be clicked.
    
    - Fix bug that displays equals sign when no operand is entered.
    
    - Fix bug that displays operator when no operand is entered.
    
    - Fix bug that makes two operators to display when clicked.
    
    - Fix bug that does not empty the first operand when clear is clicked.

--- main.js
+++ main.js
@@ -5,12 +5,13 @@ const del = document.querySelector('.del');
 const clear = document.querySelector('.clear');
 const equals = document.querySelector('.equals');
 const previousDisplay = document.querySelector('.previous-display');
+
 let selectedOperator = '';
 let secondOperand = '';
 let OperatorDisplayValue = '';
 let firstOperand = '';
 let numberDisplayValue = '';
-resetScreen = false;
+let lastAnswer = '';
 
 del.addEventListener('click', delDisplay);
 clear.addEventListener('click', clearDisplay);
@@ -47,10 +48,7 @@ function operate(operator, a, b) {
     }
 }
 
-
-
-function populateNumber() {
-    
+function populateNumber() {    // makes number to display when clicked.
     for (let number of numberBtn) {
         number.addEventListener('click', function() {
             const valueOfNumberBtn = number.getAttribute('data-nu');
@@ -61,39 +59,36 @@ function populateNumber() {
     } 
 }
 
-function populateOperator() {
+function populateOperator() {    // makes operator to display when clicked.
     operatorBtn.forEach(operator => {
         operator.addEventListener('click', function() {
+            if (currentDisplay.textContent == '' && previousDisplay.textContent == '') return
+           
             firstOperand = numberDisplayValue;
-           
             const valueOfOperator = operator.getAttribute('data-nu');
-           
-            OperatorDisplayValue += valueOfOperator;
-            
-           // currentDisplay.textContent = OperatorDisplayValue;
+            OperatorDisplayValue = valueOfOperator;
             previousDisplay.textContent = `${numberDisplayValue} ${OperatorDisplayValue}`
             numberDisplayValue = ''
-            
 
-        })
-
-    })
+            if (currentDisplay.textContent =lastAnswer) {
+                firstOperand = lastAnswer
+                previousDisplay.textContent = `${lastAnswer} ${OperatorDisplayValue}`
+                currentDisplay.textContent = ''
+            }
+        }) 
+    }) 
 }
 
-function calculate() { 
-
+function calculate() {
+    if (currentDisplay.textContent == '') return
     selectedOperator = OperatorDisplayValue;
     secondOperand = numberDisplayValue;
     console.log(firstOperand, selectedOperator, secondOperand )
-    currentDisplay.textContent = operate(selectedOperator, firstOperand, secondOperand);
+    lastAnswer = operate(selectedOperator, firstOperand, secondOperand);
+    currentDisplay.textContent = lastAnswer
     previousDisplay.textContent = `${firstOperand} ${OperatorDisplayValue} ${secondOperand} =` 
-    resetScreen = true
-    OperatorDisplayValue = '';
-}
 
-
-
-
+} 
 
 function delDisplay() {
     currentDisplay.textContent = currentDisplay.textContent
@@ -101,6 +96,7 @@ function delDisplay() {
     .slice(0, -1)
     numberDisplayValue = numberDisplayValue.toString().slice(0, -1);
     OperatorDisplayValue = OperatorDisplayValue.toString().slice(0, -1);
+
 }
     
 function clearDisplay() {
@@ -108,13 +104,9 @@ function clearDisplay() {
     numberDisplayValue = '';
     OperatorDisplayValue = '';
     previousDisplay.textContent = '';
+    lastAnswer = ''
+    firstOperand = ''
 }
-
-
-
 
 populateNumber()
 populateOperator()
-
-
-
